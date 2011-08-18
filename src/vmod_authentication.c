475bcb447c380e8ea9739b4b0ddf50547e89726d
Savetheinternet <savetheinternet@tinyboard.org>
2011-08-19 01:47:06 +1000

    ...

--- src/vmod_authentication.c
+++ src/vmod_authentication.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "vrt.h"
 #include "bin/varnishd/cache.h"
@@ -7,29 +8,73 @@
 
 #include "base64.h"
 
+#define BASE64_MAX_LEN		100
+
+typedef struct {
+	size_t username_len;
+	size_t password_len;
+	char *username;
+	char *password;
+} combination;
+
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
 	return (0);
 }
 
-const char *
-vmod_hello(struct sess *sp, const char *name)
+combination
+parse_authorization(const char *encoded)
 {
-	char *p;
-	unsigned u, v;
-
-	u = WS_Reserve(sp->wrk->ws, 0); /* Reserve some work space */
-	p = sp->wrk->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(sp->wrk->ws, 0);
-		return (NULL);
+	size_t decoded_len = BASE64_MAX_LEN;
+	char *decoded = malloc(decoded_len);
+	
+	// base64 decode
+	if(!base64_decode(encoded, strlen(encoded), decoded, &decoded_len)) {
+		// input data was invalid
 	}
-	/* Update work space with what we've used */
-	WS_Release(sp->wrk->ws, v);
-	return (p);
+	
+	combination c;
+	c.username_len = 0;
+	c.password_len = 0;
+	c.username = NULL;
+	c.password = NULL;
+	
+	int part = 0; // 0 = username; 1 = password
+		
+	for(unsigned int i = 0; i < decoded_len; i++) {
+		if(part == 0) {
+			if(decoded[i] == ':') {
+				part = 1;
+			} else {
+				c.username = realloc(c.username, (c.username_len++)+1);
+				c.username[c.username_len - 1] = decoded[i];
+				c.username[c.username_len] = 0;
+			}
+		} else {
+			c.password = realloc(c.password, (c.password_len++)+1);
+			c.password[c.password_len - 1] = decoded[i];
+			c.password[c.password_len] = 0;
+		}
+	}
+	
+	free(decoded);
+	
+	return c;
 }
+
+unsigned
+vmod_match(struct sess *sp, const char *username, const char *password, const char *encoded)
+{
+	combination c = parse_authorization(encoded);
+	
+	bool result = strcmp(c.username, username) == 0 &&strcmp(c.password, password) == 0;
+	
+	free(c.username);
+	free(c.password);
+	
+	return result;
+}
+
+
 
