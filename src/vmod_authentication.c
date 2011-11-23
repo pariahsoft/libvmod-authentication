cffd6b6484aa0036a442846e27960f8aac42bb74
Savetheinternet <savetheinternet@tinyboard.org>
2011-11-24 03:28:10 +1100

    Safter reading of files in vmod_match_file

--- src/vmod_authentication.c
+++ src/vmod_authentication.c
@@ -13,6 +13,42 @@ typedef struct{
 	char *password;
 } combination;
 
+char *_getline(FILE *fp)
+{
+	char * line = malloc(20), * linep = line;
+	size_t lenmax = 20, len = lenmax;
+	int c;
+
+	if(line == NULL)
+		return NULL;
+
+	while(1) {
+		c = fgetc(fp);
+		if(c == EOF)
+			return NULL;
+		
+		if(c == '\n')
+			break;
+		
+		if(--len == 0) {
+			char * linen = realloc(linep, lenmax *= 2);
+			len = lenmax;
+
+			if(linen == NULL) {
+				free(linep);
+				return NULL;
+			}
+			line = linen + (line - linep);
+			linep = linen;
+		}
+		
+		*line++ = c;
+	}
+	
+	*line = '\0';
+	return linep;
+}
+
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
@@ -25,8 +61,7 @@ parse_auth_header(const char *data)
 	combination *c = malloc(sizeof(combination));
 	
 	char *split = strchr(data, ':');
-	if(split == NULL)
-	{
+	if(split == NULL) {
 		// not in user:pass format
 		return NULL;
 	}
@@ -45,8 +80,7 @@ get_client_auth(struct sess *sp)
 		return NULL;
 	
 	char *split = strchr(auth_hdr, ' ');
-	if(split == NULL)
-	{
+	if(split == NULL) {
 		// invalid header data
 		return false;
 	}
@@ -57,23 +91,24 @@ get_client_auth(struct sess *sp)
 	size_t len;
 	char *decoded;
 	
-	if(!base64_decode_alloc(auth, strlen(auth), &decoded, &len))
-	{
-		// unable to allocate space for decoding base64
+	if(!base64_decode_alloc(auth, strlen(auth), &decoded, &len)) {
+		WSP(sp, SLT_VCL_Log, "vmod_authentication: unable to allocate space for decoding base64");
+		
 		free(auth);
 		return NULL;
 	}
 	
-	if(!base64_decode(auth, strlen(auth), decoded, &len))
-	{
-		// input data was seemingly invalid
+	if(!base64_decode(auth, strlen(auth), decoded, &len)) {
+		WSP(sp, SLT_VCL_Log, "vmod_authentication: unable to make sense of base64 encoded credentials");
+		
 		free(auth);
 		return NULL;
 	}
+	
 	
 	combination *c = parse_auth_header(decoded);
-	if(c == NULL)
-	{
+	
+	if(c == NULL) {
 		// something was invalid
 		free(auth);
 		return NULL;
@@ -87,16 +122,14 @@ get_client_auth(struct sess *sp)
 
 unsigned
 vmod_match(struct sess *sp, const char *username, const char *password)
-{
+{	
 	combination *c = get_client_auth(sp);
 	if(c == NULL)
 		return false;
-	
 	bool result = strcmp(c->username, username) == 0 && strcmp(c->password, password) == 0;
 	
 	free(c->username);
 	free(c->password);
-	free(c);
 	
 	return result;
 }
@@ -109,42 +142,37 @@ vmod_match_file(struct sess *sp, const char *filename)
 		return false;
 	
 	bool result = false;
-	char line[100];
 	combination *match;
 	
 	FILE* fp = fopen(filename, "r");
-	if(fp == NULL)
-	{
+	if(fp == NULL) {
 		WSP(sp, SLT_VCL_Log, "vmod_authentication: unable to open file %s", filename);
-		return false;
-	}
-	
-	while(!result && fgets(line, sizeof(line), fp))
-	{
-		if(line[strlen(line) - 1] == '\n')
-			line[strlen(line)-1] = 0;
+	} else {
+		char *line;
 		
-		// retrieve username and password for current line
-		match = parse_auth_header(line);
-		if(match == NULL)
-			continue;
-		
-		// test the combinations against eachother
-		if(strcmp(c->username, match->username) == 0 && strcmp(c->password, match->password) == 0) {
-			result = true;
+		while(!result && (line = _getline(fp)) != NULL) {
+			// retrieve username and password for current line
+			match = parse_auth_header(line);
+			if(match == NULL) {
+				free(line);
+				continue;
+			}
+			
+			// test the combinations against eachother
+			if(strcmp(c->username, match->username) == 0 && strcmp(c->password, match->password) == 0) {
+				result = true;
+			}
+			
+			free(match->username);
+			free(match->password);
+			free(match);
 		}
-		
-		free(match->username);
-		free(match->password);
-		free(match);
+		fclose(fp);
 	}
-	fclose(fp);
 	
 	free(c->username);
 	free(c->password);
-	free(c);
 	
 	return result;
 }
-
 
